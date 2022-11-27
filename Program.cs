b8911012ab0a01ce189a1c7b5da2bd9a9f4969fe
Aleksandr <lepalovskiy2015@mail.ru>
2022-11-27 12:18:39 +0700

    Задача 36

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 Задайте массив заполненный случайными положительными трёхзначными числами. 
 Напишите программу, которая покажет количество чётных чисел в массиве.
 [345, 897, 568, 234] -> 2
-*/
+
 int [] Randomarray(int size) // Заполнение массива рандомными числами от (100 до 999)
 {
     int [] newarray = new int [size];
@@ -35,26 +35,52 @@ ShowArray(array1);
 int array2 = CountUnevenNumber(array1);
 Console.WriteLine($"Count even number: {array2}");
 
-
-
-
-
-
-/*
 Задача 36: 
 Задайте одномерный массив, заполненный случайными числами.
 Найдите сумму элементов, стоящих на нечётных позициях.
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0
 
+int [] RandomArray(int size)
+{
+    int [] newArray = new int [size];
+    for (int i = 0; i < size; i++)
+    newArray [i] = new Random().Next(-100, 100);
+    return newArray;
+}
+ void ShowArray(int [] array)
+ {
+    for (int j = 0; j < array.Length; j++)
+    Console.Write(array [j] + " ");
+    Console.WriteLine(); 
+     }
+
+int SummElem(int [] summ)
+{
+    int summPos = 0;
+    for (int a = 0; a < summ.Length; a++)
+    if (a % 2 != 0)
+    {
+    summPos += summ[a];
+    }
+    return summPos;
+}
+
+Console.WriteLine("Input size of array: ");
+int size1 = Convert.ToInt32(Console.ReadLine());
+
+int[] array1 = RandomArray(size1);
+ShowArray(array1);
+
+int result = SummElem(array1);
+Console.WriteLine($"the sum of the elements standing in odd positions: {result}");
+*/
 
 
 
-
+/*
 Задача 38: 
 Задайте массив вещественных чисел. 
 Найдите разницу между максимальным и минимальным элементов массива.
 [3 7 22 2 78] -> 76
-
-
 */
